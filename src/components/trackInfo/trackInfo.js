7cf70d10d37adadf3142d5e555a3e35c894946e0
NikolayYakovenko <nikolay54yakovenko@gmail.com>
2018-06-16 19:13:18 +0300

    Added simple HTML5 player

--- src/components/trackInfo/trackInfo.js
+++ src/components/trackInfo/trackInfo.js
@@ -9,34 +9,54 @@ import './trackInfo.css';
 
 export class TrackInfo extends React.Component {
     static propTypes = {
+        loadTracks: PropTypes.func,
         tracks: PropTypes.array.isRequired,
         history: PropTypes.object.isRequired,
-    };
-
-    state = {
-        track: {},
     };
 
     componentDidMount() {
         window.scrollTo(0, 0);
 
+        const { tracks } = this.props;
+        const trackId = this.getTrackId();
+
+        if (!tracks.length) {
+            this.props.loadTracks(trackId);
+        }
+    }
+
+    getTrackId() {
         const { pathname } = this.props.history.location;
 
         let trackId = pathname.split('/');
         trackId = Number(trackId[trackId.length - 1]);
 
-        const trackData = this.props.tracks.filter((item) => {
-            return item.trackId === trackId;
-        });
-
-        this.setState({
-            track: trackData[0],
-        });
+        return trackId;
     }
 
+    getTrackById(id) {
+        const { tracks } = this.props;
+        const track = tracks.filter(item => item.trackId === id);
+
+        return track[0];
+    }
+
+    async playTrack(url) {
+        try {
+            const audio = await new Audio(url);
+            audio.play();
+
+            setTimeout(() => {
+                audio.pause();
+            }, 2000);
+        } catch (error) {
+            console.error('error:', error);
+        }
+    }
 
     render() {
-        const { track } = this.state;
+        const trackId = this.getTrackId();
+        const track = this.getTrackById(trackId);
 
         return (
             track ?
@@ -44,7 +64,7 @@ export class TrackInfo extends React.Component {
                     <div className='backLink'>
                         <Link to='' onClick={this.props.history.goBack}>Назад</Link>
                     </div>
-                    <div className='trackLogo'>
+                    <div className='logoWrapper'>
                         <img src={track.artworkUrl60} alt={track.trackName} />
                     </div>
                     <h2 className='trackName'>{track.trackName}</h2>
@@ -62,8 +82,25 @@ export class TrackInfo extends React.Component {
                             {getTrackDuration(track.trackTimeMillis)}
                         </p>
                     </div>
-                    <p><b>{track.trackPrice} {track.currency}</b></p>
-                    <button>But this track</button>
+                    <div className='details'>
+                        <p className='detailsLabel'>Price:</p>
+                        <p className='detailsInfo'>
+                            <b>{track.trackPrice} </b>{track.currency}
+                        </p>
+                    </div>
+                    <button className='controlButton'>But this track</button>
+                    <button
+                        className='controlButton'
+                        onClick={() => this.playTrack(track.previewUrl)}
+                    >
+                        Play track
+                    </button>
+                    <div className='player'>
+                        <audio tabIndex='0' id='beep' controls preload='auto'>
+                            <track kind='captions' />
+                            <source src={track.previewUrl} />
+                        </audio>
+                    </div>
                 </div>
                 : null
         );
